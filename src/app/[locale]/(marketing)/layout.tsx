dd6280baf0e680f2ef3e332dec0a833cabf35697
Julian Clarkson <julianclarkson@Mac.fios-router.home>
2025-10-24 23:05:44 -0400

    Remove duplicate generateStaticParams from marketing layout

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -1,10 +1,5 @@
 import { MarketingNav } from "@/marketing/components/MarketingNav"
 import { MarketingFooter } from "@/marketing/components/MarketingFooter"
-import { locales } from "@/i18n/config"
-
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }))
-}
 
 export default function MarketingLayout({
   children,
