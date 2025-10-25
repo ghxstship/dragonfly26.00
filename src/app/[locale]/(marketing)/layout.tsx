e4f96b88decaf2a58217bf5a29d18b4c57f5a438
Julian Clarkson <julianclarkson@Mac.fios-router.home>
2025-10-24 22:58:21 -0400

    Add generateStaticParams to marketing layout for static generation

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -1,5 +1,10 @@
 import { MarketingNav } from "@/marketing/components/MarketingNav"
 import { MarketingFooter } from "@/marketing/components/MarketingFooter"
+import { locales } from "@/i18n/config"
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }))
+}
 
 export default function MarketingLayout({
   children,
