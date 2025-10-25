2dbd7c3e32080447f7ab09f17b7cd33a12528abf
Julian Clarkson <julianclarkson@Mac.fios-router.home>
2025-10-25 00:09:26 -0400

    Force dynamic rendering for marketing pages to fix i18n client component issue

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -2,6 +2,9 @@ import { setRequestLocale } from 'next-intl/server'
 import { MarketingNav } from "@/marketing/components/MarketingNav"
 import { MarketingFooter } from "@/marketing/components/MarketingFooter"
 
+// Force dynamic rendering for marketing pages to support client components with i18n
+export const dynamic = 'force-dynamic'
+
 export default async function MarketingLayout({
   children,
   params
